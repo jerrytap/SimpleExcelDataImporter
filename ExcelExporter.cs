3be39152d4ab535867689cc3dae286c51fc5db3e
Jerry Tapolcsanyi <jerrytap@gmail.com>
2011-03-24 10:01:30 -0400

    First Push of Excel data importer and exporter

---
+++ ExcelExporter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+
+namespace GenericUtils.Data {
+    public class ExcelExporter:DataImporter {
+        private static object lockObject = new object();
+        private static string exportLocation;
+        private string tableName;
+
+        public string TableName {
+            get { return tableName; }
+            set { tableName = value; }
+        }
+        private string emptyExcelFile;
+
+        public string EmptyExcelFile {
+            get { return emptyExcelFile; }
+            set { emptyExcelFile = value; }
+        }
+
+        public static string ExportLocation {
+            get { return exportLocation; }
+            set { exportLocation = value; }
+        }
+
+
+        private string CreateEmptyExcelFile(string filename) {
+            lock (lockObject) {
+                System.IO.File.Copy(ExportLocation + "/" + EmptyExcelFile, ExportLocation + "/" + filename);
+            }
+            return ExportLocation + "/" + filename;
+        }
+
+        public void WriteDataTableToNewFile(string filename, DataTable dt) {
+            string fullFilePath = CreateEmptyExcelFile(filename);
+            WriteDataTableAsSheet(GetOleToExcelConnectionString(fullFilePath), dt);
+        }
+
+        private void WriteDataTableAsSheet(string connectionString, DataTable dt) {
+            using (OleDbConnection conn = new OleDbConnection(connectionString)) {
+                conn.Open();
+                //we decided to just use the formatted sheet in excel
+                //using (OleDbCommand cmd = conn.CreateCommand()) {
+                //    cmd.CommandText = BuildCreateSheetTabStatement(dt);
+                //    cmd.ExecuteNonQuery();
+                //}
+                foreach (DataRow dr in dt.Rows) {
+                    using (OleDbCommand cmd = conn.CreateCommand()) {
+                        cmd.CommandText = BuildInsertStatement(dr);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+            }
+        }
+
+        private string BuildInsertStatement(DataRow dr) {
+            string insertStatement = "Insert INTO [" + tableName + "$]";
+
+            StringBuilder insertColumns = new StringBuilder();
+            StringBuilder insertValues = new StringBuilder();
+
+            int colCount = dr.Table.Columns.Count;
+            if (colCount > 0) {
+
+                insertColumns.Append(" ([" + CleanStringForODBC(dr.Table.Columns[0].ColumnName));
+                insertValues.Append(" Values ('" + CleanStringForODBC(dr.GetStringFromDataRow(dr.Table.Columns[0].ColumnName, "")));
+
+                for (int currCol = 1; currCol < colCount; currCol++) {
+                    insertColumns.Append("],[" + CleanStringForODBC(dr.Table.Columns[currCol].ColumnName));
+                    insertValues.Append("','" + CleanStringForODBC(dr.GetStringFromDataRow(dr.Table.Columns[currCol].ColumnName, "")));
+
+                }
+                insertColumns.Append("])");
+                insertValues.Append("')");
+
+            }
+            return insertStatement + insertColumns.ToString() + insertValues.ToString();
+        }
+
+        private static string CleanStringForODBC(string input) {
+            if (input.Contains("'")) {
+                return input.Replace("'", "''");
+            }
+            return input;
+        }
+
+        private static string BuildCreateSheetTabStatement(DataTable dt) {
+            StringBuilder createStatement = new StringBuilder(string.Format("CREATE TABLE [{0}] ", dt.TableName));
+            int colCount = dt.Columns.Count;
+            if (colCount > 0) {
+                createStatement.Append(string.Format("([{0}] TEXT", dt.Columns[0].ColumnName));
+                for (int currCol = 1; currCol < colCount; currCol++) {
+                    createStatement.Append(string.Format(", [{0}] TEXT", dt.Columns[currCol].ColumnName));
+                }
+                createStatement.Append(")");
+            }
+            return createStatement.ToString();
+        }
+    }
+}
---
+++ ExcelImporter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GenericUtils.Data {
+    public class DataImporter {
+        private static object lockObject = new object();
+        private static string importLocation;
+
+        public static string ImportLocation {
+            get { return DataImporter.importLocation; }
+            set { DataImporter.importLocation = value; }
+        }
+
+
+        protected static string GetOleToExcelConnectionString(string pFilePath) {
+            string strConnectionString = string.Empty;
+            string strExcelExt = System.IO.Path.GetExtension(pFilePath);
+
+            if (strExcelExt == ".xls") {
+                strConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties= ""Excel 8.0;HDR=YES""";
+            } else if (strExcelExt == ".xlsx" || strExcelExt == ".xlsm") {
+                strConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0 Xml;HDR=YES""";
+            } else {
+                throw new ApplicationException("Excel file extenstion is not known.");
+            }
+            return string.Format(strConnectionString, pFilePath);
+
+        }
+
+        public static DataSet ReadFile(string filename) {
+            if (filename.ToLower().Contains(".xls")) {
+                DataSet excelFileContents = new DataSet();
+                string connectionString = GetOleToExcelConnectionString(filename);
+                List<string> sheetNames = GetSheetNames(connectionString);
+                foreach (string sheetName in sheetNames) {
+                    excelFileContents.Tables.Add(GetSheetAsDataTable(connectionString, sheetName));
+                }
+                return excelFileContents;
+            }
+         
+            throw new ApplicationException("Excel file extenstion is not known.");
+
+        }
+
+        private static void CopyColumnNamesToFirstRecord(DataTable dt) {
+            foreach (DataColumn column in dt.Columns) {
+                string newColumnName = dt.Rows[0][column].ToString();
+                if (string.IsNullOrEmpty(newColumnName)) {
+                    //column.ColumnName = ".";
+                } else {
+                    column.ColumnName = newColumnName;
+                }
+            }
+            dt.Rows.RemoveAt(0);
+        }
+
+
+
+        protected static string BuildSelect(string sheetName) {
+            return string.Format("SELECT * FROM [{0}$]", sheetName);
+        }
+
+        protected static DataTable GetSheetAsDataTable(string connectionString, string sheetName) {
+            DataTable dt = new DataTable(sheetName);
+            using (OleDbConnection conn = new OleDbConnection(connectionString)) {
+                conn.Open();
+                using (OleDbCommand cmd = conn.CreateCommand()) {
+                    cmd.CommandText = BuildSelect(sheetName);
+                    using (OleDbDataReader reader = cmd.ExecuteReader()) {
+                        if (reader.Read()) {
+                            DataTable schemaTable = reader.GetSchemaTable();
+                            foreach (DataRow row in schemaTable.Rows) {
+                                dt.Columns.Add(row[0].ToString());
+                            }
+
+                            object[] rowValues = new object[reader.FieldCount];
+                            for (int currRow = 0; currRow < reader.FieldCount; currRow++) {
+                                rowValues[currRow] = reader[currRow];
+                            }
+                            dt.Rows.Add(rowValues);
+                        }
+                        while (reader.Read()) {
+                            object[] rowValues = new object[reader.FieldCount];
+                            for (int currRow = 0; currRow < reader.FieldCount; currRow++) {
+                                rowValues[currRow] = reader[currRow];
+                            }
+                            dt.Rows.Add(rowValues);
+                        }
+                    }
+                }
+            }
+            return dt;
+        }
+
+        protected static List<string> GetSheetNames(string connectionString) {
+            List<string> sheets = new List<string>();
+            using (OleDbConnection conn = new OleDbConnection(connectionString)) {
+                conn.Open();
+                DataTable tbl = conn.GetSchema("Tables");
+                foreach (DataRow row in tbl.Rows) {
+                    string sheetName = (string)row["TABLE_NAME"];
+                    if (sheetName.EndsWith("$")) {
+                        sheetName = sheetName.Substring(0, (sheetName.Length - 1));
+                    }
+                    if (!sheets.Contains(sheetName) && !sheetName.Contains('$') ){
+                        sheets.Add(sheetName);
+                    }
+                }
+            }
+            return sheets;
+        }
+
+    }
+}
